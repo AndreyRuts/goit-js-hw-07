ac19dcb77a946ed10699472f4af723d976bd83b2
Xandr <ravnb1989@gmail.com>
2024-10-24 13:29:01 +0300

    fix hw 7.10

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,41 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const create = document.querySelector("button[data-create]");
+const destroy = document.querySelector("button[data-destroy]");
+const input = document.querySelector("input");
+const boxes = document.querySelector("#boxes");
+
+let sizes = 30;
+
+create.addEventListener("click", createMarkup);
+destroy.addEventListener("click", () => {
+  cleaner();
+})
+
+function createMarkup() {
+  if (input.value < 1 || input.value > 100) {
+    console.log("error");
+    return;
+  } 
+    createBoxes(input.value);
+}
+
+function cleaner() {
+  boxes.innerHTML = "";
+  input.value = "";
+  sizes = 30;
+}
+
+function createBoxes(amount) {
+  cleaner(); 
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${sizes}px`;
+    box.style.height = `${sizes}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.append(box);
+    sizes += 10;
+  }
+}
