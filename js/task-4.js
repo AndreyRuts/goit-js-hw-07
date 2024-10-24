38de10c11966eb50cc950b634278853ae581ab51
Xandr <ravnb1989@gmail.com>
2024-10-24 11:22:26 +0300

    fix hw 7.7

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const elements = event.currentTarget.elements;
+
+    const userData = {
+        email: elements.email.value,
+        password: elements.password.value
+    }
+    
+    event.target.reset();
+    console.log(userData);
+}
 
